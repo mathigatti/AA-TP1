a34667b957a93516ff8af2af5a3cd251c23e1e66
Emilio Gasco <gascoe@gmail.com>
2016-09-04 21:34:56 -0300

    * extraigo headers de mail para poder extraer features del subject * puse funciones de atributos en mail_attributes.py

--- baseline_example.py
+++ baseline_example.py
@@ -1,6 +1,5 @@
 # Aprendizaje Automatico - DC, FCEN, UBA
 # Segundo cuatrimestre 2016
-import re
 import json
 import numpy as np
 import pandas as pd
@@ -10,69 +9,71 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.externals.six import StringIO
 from IPython.display import Image 
 import pydotplus as pydot
+from atributes import *
 
-# Leo los mails (poner los paths correctos).
-ham_txt= json.load(open('./jsons/ham_dev.json'))
-spam_txt= json.load(open('./jsons/spam_dev.json'))
 
-# Imprimo un mail de ham y spam como muestra.
-print ham_txt[0]
-print "------------------------------------------------------"
-print spam_txt[0]
-print "------------------------------------------------------"
 
-# Armo un dataset de Pandas 
-# http://pandas.pydata.org/
+def split_mail(txt):
+    t = tuple(txt.split('\r\n\r\n',1))                          
+    if len(t) == 1:                                             
+        t = tuple(txt.split('\n\n',1))                          
+        if len(t) == 1:                                         
+            t = tuple(txt.split('\r\r',1))
+            if len(t) == 1:                                     
+                t = (txt,'<empty>')                             
+    return t[0]                                                 
+                                                                
+def attribute_ratio(df,attribute):                              
+    print attribute                                             
+    print sum(df[attribute][:len(ham_txt)])/float(len(ham_txt)) # Armo un dataset de Pandas 
+    print sum(df[attribute][len(ham_txt):])/float(len(spam_txt))# http://pandas.pydata.org/
 
-df = pd.DataFrame(ham_txt+spam_txt, columns=['text'])
-df['class'] = ['ham' for _ in range(len(ham_txt))]+['spam' for _ in range(len(spam_txt))]
+if __name__ == '__main__':
+    # Leo los mails (poner los paths correctos).
+    ham_txt= json.load(open('./jsons/ham_dev.json'))
+    spam_txt= json.load(open('./jsons/spam_dev.json'))
 
-# Extraigo dos atributos simples: 
-# 1) Longitud del mail.
-df['len'] = map(len, df.text)
+    # Imprimo un mail de ham y spam como muestra.
+    #print ham_txt[0]
+    #print "--------------headers---------------------------------"
+    #print ham_txt[0].split('\r\n\r\n',1)[0]
+    #print "--------------body----------------------------------------"
+    #print ham_txt[0].split('\r\n\r\n',1)[1]
+    #print "------------------------------------------------------"
+    #print spam_txt[0]
+    #print "------------------------------------------------------"
 
-# 2) Cantidad de espacios en el mail.
-def count_spaces(txt): return txt.count(" ")
-df['count_spaces'] = map(count_spaces, df.text)
 
-# 3) Simbolo de dolar en txt
-def has_dollar(txt): 
-    if ('$' in txt):
-        return 1
-    else:
-        return 0
-df['has_dollar'] = map(has_dollar, df.text)
+    df = pd.DataFrame(ham_txt+spam_txt, columns=['text'])
+    #df['headers'], df['body']  = zip(*df['text'].map(split_mail))
+    df['headers'] = df['text'].map(split_mail)
+    df['class'] = ['ham' for _ in range(len(ham_txt))]+['spam' for _ in range(len(spam_txt))]
+    df['len'] = map(len, df.text)
+    df['count_spaces'] = map(ma_count_spaces, df.text)
 
-# 4) has link http*://*
-def has_link(txt): 
-    found = re.match(r'.*https?://.*', txt.replace('\n','').replace(' ',''))
-    if found:
-        return 1
-    else:
-        return 0
+    df['has_dollar'] = map(ma_has_dollar, df.text)
+    attribute_ratio(df,'has_dollar')
+    df['has_link'] = map(ma_has_link, df.text)
+    attribute_ratio(df,'has_link')
+    df['has_html'] = map(ma_has_html, df.text)
+    attribute_ratio(df,'has_html')
+    df['has_cc'] = map(mha_has_cc, df.headers)
+    attribute_ratio(df,'has_cc')
+    df['has_bcc'] = map(mha_has_bcc, df.headers)
+    attribute_ratio(df,'has_bcc')
+    
 
-df['has_link'] = map(has_link, df.text)
+    # Preparo data para clasificar
+    X = df[['len', 'count_spaces','has_link','has_dollar','has_html']].values
+    y = df['class']
 
-# 4) has HTML
-def has_html(txt): 
-    found = re.match(r'.*<html>.*</html>.*', txt.lower().replace('\n','').replace(' ',''))
-    if found:
-        return 1
-    else:
-        return 0
+    # Elijo mi clasificador.
+    clf = DecisionTreeClassifier()
 
-df['has_html'] = map(has_html, df.text)
-
-# Preparo data para clasificar
-X = df[['len', 'count_spaces','has_link','has_dollar','has_html']].values
-y = df['class']
-
-# Elijo mi clasificador.
-clf = DecisionTreeClassifier()
-
-# Ejecuto el clasificador entrenando con un esquema de cross validation
-# de 10 folds.
-res = cross_val_score(clf, X, y, cv=10)
-print np.mean(res), np.std(res)
-# salida: 0.783040309346 0.0068052434174  (o similar)
+    # Ejecuto el clasificador entrenando con un esquema de cross validation
+    # de 10 folds.
+    print 'Accuracy: Mean and std dev'
+    res = cross_val_score(clf, X, y, cv=10)
+    print np.mean(res), np.std(res)
+    # salida: 0.783040309346 0.0068052434174  (o similar)
 
---
+++ mail_attributes.py
@@ -0,0 +1,50 @@
+
+import re
+
+# Extraigo dos atributos simples: 
+# 1) Longitud del mail.
+def ma_len(txt):
+    return len(txt)
+
+# 2) Cantidad de espacios en el mail.
+def ma_count_spaces(txt): 
+    return txt.count(" ")
+
+# 3) Simbolo de dolar en txt
+def ma_has_dollar(txt): 
+    if ('$' in txt):
+        return 1
+    else:
+        return 0
+
+# 4) has link http*://*
+def ma_has_link(txt): 
+    found = re.match(r'.*https?://.*', txt.replace('\n','').replace(' ',''))
+    if found:
+        return 1
+    else:
+        return 0
+
+# 5) has HTML
+def ma_has_html(txt): 
+    found = re.match(r'.*<html>.*</html>.*', txt.lower().replace('\n','').replace(' ',''))
+    if found:
+        return 1
+    else:
+        return 0
+
+# 6) has CC
+def mha_has_cc(txt): 
+    found = re.match(r'.*cc:.*', txt.lower().replace('\n','').replace(' ',''))
+    if found:
+        return 1
+    else:
+        return 0
+
+# 7) has BCC
+def mha_has_bcc(txt): 
+    found = re.match(r'.*bcc:.*', txt.lower().replace('\n','').replace(' ',''))
+    if found:
+        return 1
+    else:
+        return 0
