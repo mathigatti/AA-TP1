b581a7d7a71618e53a0147964679b845b66ac3ea
Emilio Gasco <gascoe@gmail.com>
2016-09-11 23:07:40 -0300

    * agrego helper function para poner headers de mail en dict

--- mail_attributes.py
+++ mail_attributes.py
@@ -17,14 +17,29 @@ def get_new_line_code(text):
         raise MailCorruptedException('Not able to identify new line')
 
 def get_mail_headers(mail):
-    t = tuple(mail.split('\r\n\r\n',1))
-    if len(t) == 1:
-        t = tuple(mail.split('\n\n',1))
-        if len(t) == 1:
-            t = tuple(mail.split('\r\r',1))
-            if len(t) == 1:
-                t = (mail,'')
-    return t[0]
+    header_end = mail.find('\r\n\r\n')
+    if header_end < 0:
+        header_end = mail.find('\n\n')
+        if header_end < 0:
+             header_end = mail.find('\r\r')
+    if header_end < 0:
+        headers = mail
+    else:
+        headers = mail[:header_end]
+    return headers
+
+def mail_headers_to_dict(headers):
+    headers_dict = defaultdict(str)
+    nl = get_new_line_code(headers)
+    hdr_list = headers.split(nl)
+    for line in hdr_list:
+        if line.find(':') >= 0:
+            t = line.split(':',1)
+            headers_dict[t[0].lower()] = t[1]
+            last_header = t[0].lower()
+        else:
+            headers_dict[last_header] += line;
+    return headers_dict
 
 def get_mail_body(mail):
     t = tuple(mail.split('\r\n\r\n',1))
